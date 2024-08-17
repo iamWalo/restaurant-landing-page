fc1068ab34b2a92972a9d3955535472f03c7f8ba
iamWalo <136198398+iamWalo@users.noreply.github.com>
2024-08-17 13:46:58 +0100

    Add files via upload

--- script.js
+++ script.js
@@ -1,7 +1,11 @@
+window.addEventListener('load', () => {
+    document.querySelector('.loader').style.display = 'none';
+    document.querySelector('.content').style.display = 'block';
+});
 // vedios animations
-
 const videos = document.querySelectorAll('.myVideo');
 
+// إضافة أحداث لكل فيديو
 videos.forEach(video => {
     video.addEventListener('mouseover', () => {
         video.play();
@@ -9,5 +13,6 @@ videos.forEach(video => {
 
     video.addEventListener('mouseout', () => {
         video.pause();
+        video.currentTime = 0; // إعادة الفيديو إلى البداية
     });
 });
